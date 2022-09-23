6b1b725d99d2103b75de4ae82784fc0ec0e528df
4EFI <kvv204@gmail.com>
2022-09-24 00:54:02 +0300

    Add StackDump

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 
 //---------------------------------------------------------------------------
 
+typedef unsigned long long int Uint64_t;
+
+const int TabSize = 4;
+
+//---------------------------------------------------------------------------
+
 enum StackErrors 
 {
     ERR_NULL_PTR_DATA,
@@ -19,11 +25,21 @@ struct Stack_t
     
     size_t size;
     size_t capacity;
+
+    Uint64_t errStatus;
 };
 
 //---------------------------------------------------------------------------
 
-int StackCtor (Stack_t* stack);
+int  StackCtor       (Stack_t* stack);
+int  StackErrHandler (Stack_t* stack);
+void StackDump       (Stack_t* stack);
+
+int StackPush (Stack_t* stack);    
+
+//---------------------------------------------------------------------------
+
+void PutSpaces (int numSpaces, FILE* file = stdout);
 
 //---------------------------------------------------------------------------
 
@@ -31,7 +47,9 @@ int main()
 {
     Stack_t stack = {};
 
-    StackCtor (NULL);
+    StackCtor (&stack);
+
+    StackDump (&stack);
 
     //printf ();
 }
@@ -42,11 +60,60 @@ int StackCtor (Stack_t* stack)
 { 
     Assert (stack != NULL, 0);
     
-    stack->data     = NULL;
-    stack->size     = 0;
-    stack->capacity = 0; 
+    stack->data      = NULL;
+    stack->size      = 0;
+    stack->capacity  = 0; 
+    stack->errStatus = 0;
 
     return 1;
+}
+
+//---------------------------------------------------------------------------
+
+int StackErrHandler (Stack_t* stack)
+{
+    int curErr = 0;
+
+    if (!stack->errStatus) return 0;
+
+    return 1;
+}
+
+//---------------------------------------------------------------------------
+
+void StackDump (Stack_t* stack)
+{
+    FILE* file = stderr;
+    
+    fputs ("{\n", file);
+
+    PutSpaces (TabSize, file);
+        fprintf        (file, "size     = %d;\n", stack->size);
+    PutSpaces (TabSize, file);
+        fprintf        (file, "capacity = %d;\n", stack->capacity);
+
+    PutSpaces (2*TabSize, file);
+        fputs ("{\n",     file);
+
+    for (int i = 0; i < stack->size; i++)
+    {
+    PutSpaces (2*TabSize, file);
+            fprintf      (file, "%s[%d] = %d", 0 ? " " : "*", i, stack->data[i]);
+    }
+
+    PutSpaces (2*TabSize, file);
+        fputs ("}\n",     file);
+
+    fputs ("}\n", file);
+}
+
+//---------------------------------------------------------------------------
+
+void PutSpaces (int numSpaces, FILE* file)
+{
+    Assert (file != NULL);
+
+    fprintf (file, "%*s", TabSize, "");
 }
 
 //---------------------------------------------------------------------------
